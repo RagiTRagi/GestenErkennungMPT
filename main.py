f7a089db5b5dd1015b8895d8b18a1e43b4e764ec
Rafael Lotz <rafael.lotz@study.hs-duesseldorf.de>
2026-07-14 20:30:23 +0200

    Neue Befehle für aufnahme und schnelles trainieren ohne optuna

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import argparse
 
 from GestureRecognition import run
-
-parser = argparse.ArgumentParser("GestureRecognition")
+from GestureRecognition.labeling import data_labeling
 
 def main():
+    """Startet wahlweise die Aufnahme oder die Gestenerkennung."""
+    parser = argparse.ArgumentParser("GestureRecognition")
+    parser.add_argument(
+        "--record",
+        action="store_true",
+        help="Startet die interaktive Aufnahme und Beschriftung von Gesten.",
+    )
+
+    args, _ = parser.parse_known_args()
+    if args.record:
+        data_labeling()
+        return
+
     run(parser)
 
 if __name__ == "__main__":
